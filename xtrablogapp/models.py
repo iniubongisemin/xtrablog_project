7905ba4f2de0464f1049923483d9b1abeb497c38
Ini-ubong Isemin <150362719+iniubongisemin@users.noreply.github.com>
2024-01-29 11:49:16 +0100

    Update models.py

--- xtrablogapp/models.py
+++ xtrablogapp/models.py
@@ -33,7 +33,18 @@ class Post(models.Model):
     published_date = models.DateField(auto_now_add=True)
     author = models.CharField(max_length=100)
     thumbnail = models.ImageField(upload_to='thumbnail', default='example.png')
-    category = models.ForeignKey(Category, )
-
+    # ForeignKey is used to create a many to one relationship
+    category = models.ForeignKey(Category, related_name='articles, on_delete=models.CASCADE, default=1) 
+    # when you delete an object in a category then the other objects connected to it are also deleted
+    # category = models.ForeignKey(Category, on_delete=models.PROTECT) # when you attempt to delete an object in a category with other objects connected to it, it prevents the delete action! 
+    # category = models.ForeignKey(Category, on_delete=models.DO_NOTHING) # when you delete an object in a category then nothing is done to the other objects connected to it 
+    # category = models.ForeignKey(Category, on_delete=models.SET_NULL) # when you delete an object in a category then the other objects connected to it are set to null PS: you must set null=True  
+    tags = models.ManyToManyField(Tag)
+    
     def __str__(self) -> str:
         return self.title
+
+# steps
+# python manage.py makemigrations
+# python manage.py migrate
+# open admin panel and 
